e67375e639bd1d95dd717fe5ec25a86f5a6b5d7b
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2026-03-26 06:15:31 +0800

    logout authentication function added

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -50,7 +50,11 @@ class AuthController extends Controller
 
     public function logout(Request $request) 
     {
+        $user = $request->user();
 
+        $user->currentAccessToken()->delete();
+
+        return response('',204);
     }
 
     
