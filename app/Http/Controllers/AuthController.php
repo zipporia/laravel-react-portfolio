78e17410e0cfd578efa1f31fa4137c4876fb4c39
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2026-03-26 05:39:21 +0800

    api file installed

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request) 
+    {
+
+    }
+
+    public function register(RegisterRequest $request) 
+    {
+
+    }
+
+    public function logout(Request $request) 
+    {
+
+    }
+
+    
+}
