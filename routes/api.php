1f8f816cdee4b2f87b686ef42db92e13075cdae2
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2026-03-29 06:26:26 +0800

    fixed api route

--- routes/api.php
+++ routes/api.php
@@ -4,11 +4,20 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// single middleware
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout']);
+// Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout']);
+
+// group middleware
+Route::middleware('auth:sanctum')->group(function() {
+    Route::get('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', function (Request $request) {
+    return $request->user();
+    });
+});
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
