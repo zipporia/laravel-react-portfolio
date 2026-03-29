754b095838356376fd7b7b5536ff6c14373c134f
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2026-03-29 14:42:19 +0800

    added user controller, store and update request

--- laravel-react-portfolio/src/views/Users.jsx
+++ laravel-react-portfolio/src/views/Users.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react"
+import axiosClient from "../axiosClient"
+
 export default function Users() {
+
+    const [users , setUsers] = useState ([])
+
+    useEffect(() => {
+        axiosClient.get('/user').then(({data}) => {
+            // console.log(data)
+            setUsers(data)
+        })
+    },[])
+
     return (
         <>
             <div>
