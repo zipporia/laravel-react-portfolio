b2914314ebe03d84f0bf2b09a8f5d040394b480b
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2026-03-30 23:47:24 +0800

    user form fixed  bugs in payload

--- laravel-react-portfolio/src/views/UserForm.jsx
+++ laravel-react-portfolio/src/views/UserForm.jsx
@@ -1,27 +1,33 @@
-import { useEffect, useRef } from "react"
+
+import { useRef } from "react";
 import axiosClient from "../axiosClient"
 
 export default function UserForm(){
 
     const nameRef = useRef();
-
-    const payload = {
-        name: nameRef.current.value,
-    }
+    const emailRef = useRef();
+    const passwordRef = useRef();
 
     const Submit = (ev) => {
         ev.preventDefault();
-        axiosClient.post('/users')
+
+        const payload = {
+            name: nameRef.current.value,
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+        }
+        // axiosClient.post('/users')
+        console.log(payload)
     }
 
     return(
         <div>
             User Form
             <form onSubmit={Submit} >
-                <input ref={nameRef} type="name" />
-                <input type="email" />
-                <input type="password" />
-                <button className="btn btn-block">Submit</button>
+                <input ref={nameRef} type="text" placeholder="Name" />
+                <input ref={emailRef}  type="email" placeholder="Email" />
+                <input ref={passwordRef}  type="password" placeholder="Password" />
+                <button type="submit" className="btn btn-block">Submit</button>
             </form>
 
         </div>
