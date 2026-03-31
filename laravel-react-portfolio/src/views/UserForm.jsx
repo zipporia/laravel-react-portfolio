66c53ed29defcd3ad621dde471fac243892701a1
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2026-04-01 05:35:10 +0800

    user form add form function

--- laravel-react-portfolio/src/views/UserForm.jsx
+++ laravel-react-portfolio/src/views/UserForm.jsx
@@ -1,34 +1,53 @@
 
 import { useRef } from "react";
 import axiosClient from "../axiosClient"
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
 
 export default function UserForm(){
 
-    const nameRef = useRef();
-    const emailRef = useRef();
-    const passwordRef = useRef();
+    const {id} = useParams();
+    const [users, setUsers] = useState({
+        id: null,
+        name: '',
+        email: '',
+        password: ''
+    })
+    const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState(null);
+
+    if(id){
+        useEffect(() => {
+            setLoading(true)
+            axiosClient.get(`/users/${id}`)
+                .then(({data}) => {
+                    setLoading(false)
+                    setUsers(data)
+                })
+                .catch(() => {
+                    setLoading(false)
+                })
+        },[])
+    }
 
     const Submit = (ev) => {
         ev.preventDefault();
 
-        const payload = {
-            name: nameRef.current.value,
-            email: emailRef.current.value,
-            password: passwordRef.current.value,
-        }
-        // axiosClient.post('/users')
-        console.log(payload)
+        console.log(users)
+    
     }
 
     return(
         <div>
-            User Form
-            <form onSubmit={Submit} >
-                <input ref={nameRef} type="text" placeholder="Name" />
-                <input ref={emailRef}  type="email" placeholder="Email" />
-                <input ref={passwordRef}  type="password" placeholder="Password" />
-                <button type="submit" className="btn btn-block">Submit</button>
-            </form>
+            {!loading && (
+                <form onSubmit={Submit} >
+                    <input value={users.name} onChange={ev => setUsers({...users, name: ev.target.value})} type="text" placeholder="Name" />
+                    <input type="email" placeholder="Email" />
+                    <input type="password" placeholder="Password" />
+                    <button type="submit" className="btn btn-block">Submit</button>
+                </form>
+            )}
+
 
         </div>
     )
