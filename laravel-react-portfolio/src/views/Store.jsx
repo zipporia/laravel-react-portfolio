5e04f337d096fac48255254f2157fb12ae249969
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2026-04-06 23:28:28 +0800

    fixed bugs in store product

--- laravel-react-portfolio/src/views/Store.jsx
+++ laravel-react-portfolio/src/views/Store.jsx
@@ -1,25 +1,18 @@
-
 import { Link } from 'react-router-dom';
 
-import '../StoreAssets/styles/shared/general.css'
-import '../StoreAssets/styles/shared/header.css'
-import '../StoreAssets/styles/pages/index.css'
+import '../StoreAssets/styles/pages/index.css';
+import '../StoreAssets/styles/shared/general.css';
+import '../StoreAssets/styles/shared/header.css';
 
-import logo from '../StoreAssets/images/logo-white.png';
-import mobileLogo from '../StoreAssets/images/mobile-logo-white.png';
-import searchIcon from '../StoreAssets/images/icons/search-icon.png';
 import cartIcon from '../StoreAssets/images/icons/cart-icon.png';
 
-import { products } from '../../starting-code/data/products';
 import { useEffect } from 'react';
-
-
+import { products } from '../../starting-code/data/products';
 
 export default function Store() {
-
     useEffect(() => {
-    console.log(products)
-},[])
+        console.log(products);
+    }, []);
 
     return (
         <>
@@ -44,72 +37,61 @@ export default function Store() {
 
                 <div className="right-section">
                     <Link className="orders-link header-link" to="/orders">
-
-                    <span className="orders-text">Orders</span>
+                        <span className="orders-text">Orders</span>
                     </Link>
 
                     <Link className="cart-link header-link" to="/checkout">
-                    <img className="cart-icon" src={cartIcon} />
-                    <div className="cart-quantity">3</div>
-                    <div className="cart-text">Cart</div>
+                        <img className="cart-icon" src={cartIcon} />
+                        <div className="cart-quantity">3</div>
+                        <div className="cart-text">Cart</div>
                     </Link>
                 </div>
-                </div>
+            </div>
 
-                <div className="home-page">
+            <div className="home-page">
                 <div className="products-grid">
+                    {products.map((product) => {
+                        return (
+                            <div key={product.id} className="product-container">
+                                <div className="product-image-container">
+                                    <img className="product-image" src="StoreAssets/images/products/athletic-cotton-socks-6-pairs.jpg" />
+                                </div>
 
-                    {products.map(product => (
-                        <div className="product-container">
-                        <div className="product-image-container">
-                            <img className="product-image"
-                            src="StoreAssets/images/products/athletic-cotton-socks-6-pairs.jpg" />
-                        </div>
+                                <div className="product-name limit-text-to-2-lines">{product.name}</div>
 
-                        <div className="product-name limit-text-to-2-lines">
-                            {product.name}
-                        </div>
+                                <div className="product-rating-container">
+                                    <img className="product-rating-stars" src="StoreAssets/images/ratings/rating-45.png" />
+                                    <div className="product-rating-count link-primary">87</div>
+                                </div>
 
-                        <div className="product-rating-container">
-                            <img className="product-rating-stars"
-                            src="StoreAssets/images/ratings/rating-45.png" />
-                            <div className="product-rating-count link-primary">
-                            87
+                                <div className="product-price">$10.90</div>
+
+                                <div className="product-quantity-container">
+                                    <select>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                    </select>
+                                </div>
+
+                                <div className="product-spacer"></div>
+
+                                <div className="added-to-cart">
+                                    <img src="StoreAssets/images/icons/checkmark.png" />
+                                    Added
+                                </div>
+
+                                <button className="add-to-cart-button button-primary">Add to Cart</button>
                             </div>
-                        </div>
-
-                        <div className="product-price">
-                            $10.90
-                        </div>
-
-                        <div className="product-quantity-container">
-                            <select>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                            <option value="6">6</option>
-                            <option value="7">7</option>
-                            <option value="8">8</option>
-                            <option value="9">9</option>
-                            <option value="10">10</option>
-                            </select>
-                        </div>
-
-                        <div className="product-spacer"></div>
-
-                        <div className="added-to-cart">
-                            <img src="StoreAssets/images/icons/checkmark.png" />
-                            Added
-                        </div>
-
-                        <button className="add-to-cart-button button-primary">
-                            Add to Cart
-                        </button>
-                        </div>
-                    ))}
- 
+                        );
+                    })}
 
                     {/* <div className="product-container">
                     <div className="product-image-container">
@@ -211,5 +193,5 @@ export default function Store() {
                 </div>
             </div>
         </>
-    )
+    );
 }
