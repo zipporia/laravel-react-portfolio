e1351033cb6a1bf21116d47652f1bc042f8a984e
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2026-04-06 23:24:55 +0800

    add product in store

--- laravel-react-portfolio/src/views/Store.jsx
+++ laravel-react-portfolio/src/views/Store.jsx
@@ -10,9 +10,17 @@ import mobileLogo from '../StoreAssets/images/mobile-logo-white.png';
 import searchIcon from '../StoreAssets/images/icons/search-icon.png';
 import cartIcon from '../StoreAssets/images/icons/cart-icon.png';
 
+import { products } from '../../starting-code/data/products';
+import { useEffect } from 'react';
+
 
 
 export default function Store() {
+
+    useEffect(() => {
+    console.log(products)
+},[])
+
     return (
         <>
             <title>Store</title>
@@ -50,56 +58,60 @@ export default function Store() {
 
                 <div className="home-page">
                 <div className="products-grid">
-                    <div className="product-container">
-                    <div className="product-image-container">
-                        <img className="product-image"
-                        src="StoreAssets/images/products/athletic-cotton-socks-6-pairs.jpg" />
-                    </div>
 
-                    <div className="product-name limit-text-to-2-lines">
-                        Black and Gray Athletic Cotton Socks - 6 Pairs
-                    </div>
-
-                    <div className="product-rating-container">
-                        <img className="product-rating-stars"
-                        src="StoreAssets/images/ratings/rating-45.png" />
-                        <div className="product-rating-count link-primary">
-                        87
+                    {products.map(product => (
+                        <div className="product-container">
+                        <div className="product-image-container">
+                            <img className="product-image"
+                            src="StoreAssets/images/products/athletic-cotton-socks-6-pairs.jpg" />
                         </div>
-                    </div>
 
-                    <div className="product-price">
-                        $10.90
-                    </div>
+                        <div className="product-name limit-text-to-2-lines">
+                            {product.name}
+                        </div>
 
-                    <div className="product-quantity-container">
-                        <select>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        </select>
-                    </div>
+                        <div className="product-rating-container">
+                            <img className="product-rating-stars"
+                            src="StoreAssets/images/ratings/rating-45.png" />
+                            <div className="product-rating-count link-primary">
+                            87
+                            </div>
+                        </div>
 
-                    <div className="product-spacer"></div>
+                        <div className="product-price">
+                            $10.90
+                        </div>
 
-                    <div className="added-to-cart">
-                        <img src="StoreAssets/images/icons/checkmark.png" />
-                        Added
-                    </div>
+                        <div className="product-quantity-container">
+                            <select>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                            <option value="6">6</option>
+                            <option value="7">7</option>
+                            <option value="8">8</option>
+                            <option value="9">9</option>
+                            <option value="10">10</option>
+                            </select>
+                        </div>
 
-                    <button className="add-to-cart-button button-primary">
-                        Add to Cart
-                    </button>
-                    </div>
+                        <div className="product-spacer"></div>
 
-                    <div className="product-container">
+                        <div className="added-to-cart">
+                            <img src="StoreAssets/images/icons/checkmark.png" />
+                            Added
+                        </div>
+
+                        <button className="add-to-cart-button button-primary">
+                            Add to Cart
+                        </button>
+                        </div>
+                    ))}
+ 
+
+                    {/* <div className="product-container">
                     <div className="product-image-container">
                         <img className="product-image"
                         src="StoreAssets/images/products/intermediate-composite-basketball.jpg" />
@@ -195,7 +207,7 @@ export default function Store() {
                     <button className="add-to-cart-button button-primary">
                         Add to Cart
                     </button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
