771d49a216fe301876bc0ecae908e6c9f5a09140
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2026-04-09 06:31:29 +0800

    added product controller, reqeust, resource

--- laravel-react-portfolio/src/views/Store.jsx
+++ laravel-react-portfolio/src/views/Store.jsx
@@ -85,7 +85,7 @@ export default function Store() {
                                 <div className="product-spacer"></div>
 
                                 <div className="added-to-cart">
-                                    <img src="StoreAssets/images/icons/checkmark.png" />
+                                    <img src="/images/icons/checkmark.png" />
                                     Added
                                 </div>
 
@@ -94,103 +94,6 @@ export default function Store() {
                         );
                     })}
 
-                    {/* <div className="product-container">
-                    <div className="product-image-container">
-                        <img className="product-image"
-                        src="StoreAssets/images/products/intermediate-composite-basketball.jpg" />
-                    </div>
-
-                    <div className="product-name limit-text-to-2-lines">
-                        Intermediate Size Basketball
-                    </div>
-
-                    <div className="product-rating-container">
-                        <img className="product-rating-stars"
-                        src="StoreAssets/images/ratings/rating-40.png" />
-                        <div className="product-rating-count link-primary">
-                        127
-                        </div>
-                    </div>
-
-                    <div className="product-price">
-                        $20.95
-                    </div>
-
-                    <div className="product-quantity-container">
-                        <select>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        </select>
-                    </div>
-
-                    <div className="product-spacer"></div>
-
-                    <div className="added-to-cart">
-                        <img src="StoreAssets/images/icons/checkmark.png" />
-                        Added
-                    </div>
-
-                    <button className="add-to-cart-button button-primary">
-                        Add to Cart
-                    </button>
-                    </div>
-
-                    <div className="product-container">
-                    <div className="product-image-container">
-                        <img className="product-image"
-                        src="StoreAssets/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
-                    </div>
-
-                    <div className="product-name limit-text-to-2-lines">
-                        Adults Plain Cotton T-Shirt - 2 Pack
-                    </div>
-
-                    <div className="product-rating-container">
-                        <img className="product-rating-stars"
-                        src="StoreAssets/images/ratings/rating-45.png" />
-                        <div className="product-rating-count link-primary">
-                        56
-                        </div>
-                    </div>
-
-                    <div className="product-price">
-                        $7.99
-                    </div>
-
-                    <div className="product-quantity-container">
-                        <select>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        <option value="4">4</option>
-                        <option value="5">5</option>
-                        <option value="6">6</option>
-                        <option value="7">7</option>
-                        <option value="8">8</option>
-                        <option value="9">9</option>
-                        <option value="10">10</option>
-                        </select>
-                    </div>
-
-                    <div className="product-spacer"></div>
-
-                    <div className="added-to-cart">
-                        <img src="StoreAssets/images/icons/checkmark.png" />
-                        Added
-                    </div>
-
-                    <button className="add-to-cart-button button-primary">
-                        Add to Cart
-                    </button>
-                    </div> */}
                 </div>
             </div>
         </>
