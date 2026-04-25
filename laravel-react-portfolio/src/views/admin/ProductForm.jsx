7792edbbbac1416f887b2c0904742b898ae49a64
Mark Christopher Cornelio <39235691+zipporia@users.noreply.github.com>
2026-04-25 18:31:16 +0800

    update product form

--- laravel-react-portfolio/src/views/admin/ProductForm.jsx
+++ laravel-react-portfolio/src/views/admin/ProductForm.jsx
@@ -10,7 +10,7 @@ export default function ProductForm() {
 
                         <div className="col-span-full">
                             <label htmlFor="photo" className="block text-sm/6 font-medium text-gray-900">
-                                Product Image
+                                Product Photo
                             </label>
                             <div className="mt-2 flex items-center gap-x-3">
                                 <UserCircleIcon aria-hidden="true" className="size-12 text-gray-300" />
@@ -29,13 +29,13 @@ export default function ProductForm() {
 
                     <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                         <div className="sm:col-span-6">
-                            <label htmlFor="first-name" className="block text-sm/6 font-medium text-gray-900">
-                                First name
+                            <label htmlFor="product-name" className="block text-sm/6 font-medium text-gray-900">
+                                Product name
                             </label>
                             <div className="mt-2">
                                 <input
-                                    id="first-name"
-                                    name="first-name"
+                                    id="product-name"
+                                    name="product-name"
                                     type="text"
                                     autoComplete="given-name"
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
@@ -44,13 +44,13 @@ export default function ProductForm() {
                         </div>
 
                         <div className="sm:col-span-6">
-                            <label htmlFor="last-name" className="block text-sm/6 font-medium text-gray-900">
-                                Last name
+                            <label htmlFor="rating-stars" className="block text-sm/6 font-medium text-gray-900">
+                                Rating Stars
                             </label>
                             <div className="mt-2">
                                 <input
-                                    id="last-name"
-                                    name="last-name"
+                                    id="rating-stars"
+                                    name="rating-stars"
                                     type="text"
                                     autoComplete="family-name"
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
@@ -58,35 +58,51 @@ export default function ProductForm() {
                             </div>
                         </div>
 
-                        <div className="sm:col-span-3">
-                            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
-                                Email address
+                        <div className="sm:col-span-6">
+                            <label htmlFor="rating-count" className="block text-sm/6 font-medium text-gray-900">
+                                Rating Count
                             </label>
                             <div className="mt-2">
                                 <input
-                                    id="email"
-                                    name="email"
-                                    type="email"
+                                    id="rating-count"
+                                    name="rating-count"
+                                    type="text"
+                                    autoComplete="family-name"
+                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                />
+                            </div>
+                        </div>
+
+
+                        <div className="sm:col-span-6">
+                            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
+                                Price Cents
+                            </label>
+                            <div className="mt-2">
+                                <input
+                                    id="price_cents"
+                                    name="price_cents"
+                                    type="number"
                                     autoComplete="email"
                                     className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 />
                             </div>
                         </div>
 
-                        <div className="sm:col-span-3">
-                            <label htmlFor="country" className="block text-sm/6 font-medium text-gray-900">
-                                Country
+                        <div className="sm:col-span-6">
+                            <label htmlFor="keywords" className="block text-sm/6 font-medium text-gray-900">
+                                Keywords
                             </label>
                             <div className="mt-2 grid grid-cols-1">
                                 <select
-                                    id="country"
-                                    name="country"
+                                    id="keywords"
+                                    name="keywords"
                                     autoComplete="country-name"
                                     className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 >
-                                    <option>United States</option>
-                                    <option>Canada</option>
-                                    <option>Mexico</option>
+                                    <option>Sport</option>
+                                    <option>Apparel</option>
+                                    <option>Men's</option>
                                 </select>
                                 <ChevronDownIcon
                                     aria-hidden="true"
